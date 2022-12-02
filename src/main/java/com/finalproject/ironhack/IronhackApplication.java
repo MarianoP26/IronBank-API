37cd20f46024860f65d14c4969365a58ae2cc711
MarianoP26 <marianopierretti20@hotmail.com>
2022-12-02 12:58:57 +0100

    init

---
+++ src/main/java/com/finalproject/ironhack/IronhackApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.ironhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronhackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/ironhack/IronhackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.ironhack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronhackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
